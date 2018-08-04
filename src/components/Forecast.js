f2843744b9629c53d600c5a9218dc4e4a326e2fa
Kaeside Iwuagwu <kaeside.iwuagwu@gmail.com>
2018-08-04 16:30:47 -0400

    renames summary prop on DailyForecast component

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -51,7 +51,7 @@ class Forecast extends Component {
         return (
         <div className="forecast">
             <CurrentForecast currentForecast={this.state.currentForecast} />
-            {this.state.dailyForecast.map((item, i) => <DailyForecast key={i} summary={item.summary} />)}
+            {this.state.dailyForecast.map((forecast, i) => <DailyForecast key={i} summary={forecast} />)}
         </div>
         )
     }
