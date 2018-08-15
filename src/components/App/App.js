af994210e583042ae87065873b4487d55c2a9eaa
Kaeside Iwuagwu <kaeside.iwuagwu@gmail.com>
2018-08-15 03:49:15 -0400

    moves Forecast component functions up to App component. SearchModal component is now rendered in App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,73 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+import {geocodeByAddress, getLatLng} from 'react-places-autocomplete';
 import logo from '../../assets/logos/logo.svg';
 import './App.css';
 import Forecast from '../Forecast/Forecast';
+import SearchModal from '../SearchModal/SearchModal';
 
 class App extends Component {
+  state = {
+      lat: 36.081944,
+      lng: -115.124722,
+      formattedAddress: 'Paradise, NV 89119, USA',
+      currentForecast: {
+          summary: 'Weather Summary',
+          temperature: 0,
+      },
+      dailyForecast: [
+          {
+              summary: 'Weather Summary',
+              temperature: 0,
+              temperatureLow: 0,
+              temperatureHigh: 0
+          }
+      ],
+      units: 'auto',
+      isLoading: true
+  }
+  componentDidMount() {
+        this.fetchForecast(this.state.lat, this.state.lng);
+  }
+  handleLocationTextChange = (address) => {
+      this.setState({address})
+  }
+  handleLocationSelection = async (address) => {
+      try {
+          let results = await geocodeByAddress(address)
+          this.setState({
+              formattedAddress: results[0].formatted_address
+          })
+          let coord = await getLatLng(results[0])
+          this.setState({
+              lat: coord.lat,
+              lng: coord.lng,
+              isLoading: true
+          })
+          await this.fetchForecast(coord.lat, coord.lng);
+      } catch (error) {
+          console.error('Error', error)
+      } 
+  }
+  fetchForecast = (lat, lng) => {
+      axios.get(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${process.env.REACT_APP_DARK_SKY_API_KEY}/${lat},${lng}`, {
+          params: {
+              units: this.state.units
+          }
+      })
+      .then(response => {
+          this.setState((prevState, props) => {
+              return {
+                  lat: response.data.latitude, 
+                  lng: response.data.longitude, 
+                  currentForecast: response.data.currently, 
+                  dailyForecast: [...response.data.daily.data],
+                  isLoading: false
+              }
+          })
+      })
+      .catch(error => console.error(error))
+  }
   render() {
     return (
       <div className="App">
@@ -11,7 +75,21 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Welcome to Ubochi!</h1>
         </header>
-        <Forecast />
+        <SearchModal
+          handleLocationTextChange={this.handleLocationTextChange}
+          handleLocationSelection={this.handleLocationSelection} 
+          fetchForecast={this.fetchForecast}
+        />
+        <Forecast 
+          fetchForecast={this.fetchForecast}
+          dailyForecast={this.state.dailyForecast}
+          isLoading={this.state.isLoading}
+          lat={this.state.lat}
+          lng={this.state.lng}
+        />
+      <footer>
+        <p>{this.state.formattedAddress}</p>
+      </footer>
       </div>
     );
   }
--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -1,33 +1,8 @@
 import React, {Component} from 'react';
-import axios from 'axios';
-import {geocodeByAddress, getLatLng} from 'react-places-autocomplete';
 import DailyForecast from '../Forecast/DailyForecast/DailyForecast';
-import SearchModal from '../SearchModal/SearchModal';
 import Loader from '../Loader/Loader';
 
 class Forecast extends Component {
-    state = {
-        lat: 36.081944,
-        lng: -115.124722,
-        formattedAddress: 'Paradise, NV 89119, USA',
-        currentForecast: {
-            summary: 'Weather Summary',
-            temperature: 0,
-        },
-        dailyForecast: [
-            {
-                summary: 'Weather Summary',
-                temperature: 0,
-                temperatureLow: 0,
-                temperatureHigh: 0
-            }
-        ],
-        units: 'auto',
-        isLoading: true
-    }
-    componentDidMount() {
-        this.fetchForecast(this.state.lat, this.state.lng);
-    }
     getDay = (time) => {
         const dayStamp = new Date(time*1000).getDay();
         const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
@@ -37,57 +12,17 @@ class Forecast extends Component {
             return 'Weekday'
         }
     }
-    handleLocationTextChange = (address) => {
-        this.setState({address})
-    }
-    handleLocationSelection = async (address) => {
-        try {
-            let results = await geocodeByAddress(address)
-            this.setState({
-                formattedAddress: results[0].formatted_address
-            })
-            let coord = await getLatLng(results[0])
-            this.setState({
-                lat: coord.lat,
-                lng: coord.lng
-            })
-            await this.fetchForecast(coord.lat, coord.lng);
-        } catch (error) {
-            console.error('Error', error)
-        } 
-    }
+
     getTemperature = (temp) => {
         return Math.round(temp)
     }
-    fetchForecast = (lat, lng) => {
-        axios.get(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${process.env.REACT_APP_DARK_SKY_API_KEY}/${lat},${lng}`, {
-            params: {
-                units: this.state.units
-            }
-        })
-        .then(response => {
-            this.setState((prevState, props) => {
-                return {
-                    lat: response.data.latitude, 
-                    lng: response.data.longitude, 
-                    currentForecast: response.data.currently, 
-                    dailyForecast: [...response.data.daily.data],
-                    isLoading: false
-                }
-            })
-        })
-        .catch(error => console.error(error))
-    }
+    
     render() {
+        const { dailyForecast, isLoading } = this.props;
         return (
         <div className="forecast">
-            <SearchModal
-                handleLocationTextChange={this.handleLocationTextChange}
-                handleLocationSelection={this.handleLocationSelection} 
-                fetchForecast={this.fetchForecast}
-            />
             <div className="weekly-forecast">
-                {this.state.isLoading ? <Loader /> : this.state.dailyForecast.slice(0,5).map((forecast, index) => {
+                {isLoading ? <Loader /> : dailyForecast.slice(0,5).map((forecast, index) => {
                     return <DailyForecast 
                                 key={index} 
                                 icon={forecast.icon}
@@ -98,7 +33,6 @@ class Forecast extends Component {
                     })
                 }
             </div>
-            <p>{this.state.formattedAddress}</p>
         </div>
         )
     }
